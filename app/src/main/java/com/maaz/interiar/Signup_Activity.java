f37ea0c7e3878e38311e115759708b0670051f94
Hira Ibrahim <hiraibrahim197@gmail.com>
2019-08-02 00:28:40 +0500

    -Signup Activity commit

--- app/src/main/java/com/maaz/interiar/Signup_Activity.java
+++ app/src/main/java/com/maaz/interiar/Signup_Activity.java
@@ -6,30 +6,31 @@ import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
-public class Login_Activity extends AppCompatActivity {
+public class Signup_Activity extends AppCompatActivity {
 
     private Switch aSwitch;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_);
+        setContentView(R.layout.activity_signup_);
 
         aSwitch= (Switch) findViewById(R.id.switch_button);
+
 
         aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                if(isChecked==true)
+                if(isChecked==false)
                 {
-                    Intent intent = new Intent(Login_Activity.this,Signup_Activity.class);
+                    Intent intent = new Intent(Signup_Activity.this,Login_Activity.class);
                     startActivity(intent);
                     finish();
-                }
 
+                }
             }
         });
+
     }
 }
