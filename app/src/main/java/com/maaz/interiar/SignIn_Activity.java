65f16096fc6662300fda441623a052a3c378c051
Hira Ibrahim <hiraibrahim197@gmail.com>
2019-08-28 20:35:37 +0500

    - Hira push

--- app/src/main/java/com/maaz/interiar/SignIn_Activity.java
+++ app/src/main/java/com/maaz/interiar/SignIn_Activity.java
@@ -1,43 +1,14 @@
 package com.maaz.interiar;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.graphics.Region;
-import android.nfc.Tag;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.maaz.interiar.Model.Users;
-import com.maaz.interiar.Prevalent.Prevalent;
-
-import io.paperdb.Paper;
 
 public class SignIn_Activity extends AppCompatActivity {
 
@@ -45,12 +16,9 @@ public class SignIn_Activity extends AppCompatActivity {
     private RadioButton radioSigninBtn, radioSignupBtn;
     private TextView forgetPassword, divider_text;
     private EditText signInEmail, signInPassword, signUpName, signUpEmail, signUpPassword;
-    private Button loginButton, googleButton, facebookButton;
-    private ProgressDialog loadingBar;
-    //private String parentDBName = "Users";
-    private FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
-    private static final int RC_SIGN_IN = 9001;
+    private Button loginButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,35 +35,10 @@ public class SignIn_Activity extends AppCompatActivity {
         signUpName = (EditText) findViewById(R.id.signUp_name);
         signUpEmail = (EditText) findViewById(R.id.signUp_email);
         signUpPassword = (EditText) findViewById(R.id.signUp_password);
+
         loginButton = (Button) findViewById(R.id.login_btn);
-        googleButton = findViewById(R.id.google_button);
-        facebookButton = findViewById(R.id.fb_button);
-        loadingBar = new ProgressDialog(this);
-        //Paper.init(this);
 
-        mAuth = FirebaseAuth.getInstance();
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .requestProfile()
-                .requestId()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if (currentUser != null || account != null)
-        {
-            loadingBar.setTitle("Already Logged in");
-            loadingBar.setMessage("Please wait...");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-            Intent intent = new Intent(SignIn_Activity.this, Home_Activity.class);
-            startActivity(intent);
-            loadingBar.dismiss();
-        }
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -128,150 +71,10 @@ public class SignIn_Activity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                if (radioSigninBtn.isChecked())
-                {
-                    SignInUser();
-                }
-                else if (radioSignupBtn.isChecked())
-                {
-                    SignUpUser();
-                }
-            }
-        });
-
-        googleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SignInWithGoogle();
-            }
-        });
-    }
-
-    private void SignUpUser()
-    {
-        String email = signUpEmail.getText().toString();
-        String password = signUpPassword.getText().toString();
-
-        if(TextUtils.isEmpty(email))
-        {
-            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
-            signUpEmail.requestFocus();
-        }
-        else if (TextUtils.isEmpty(password))
-        {
-            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
-            signUpPassword.requestFocus();
-        }
-        else
-        {
-            loadingBar.setTitle("Creating Account");
-            loadingBar.setMessage("Please wait. While we are checking credentials");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-
-            mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(SignIn_Activity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful())
-                            {
-                                //AddUserToDatabase();
-                                Intent intent = new Intent(SignIn_Activity.this, Home_Activity.class);
-                                startActivity(intent);
-                            }
-                            else
-                            {
-                                Toast.makeText(SignIn_Activity.this, "SignUp UnSuccessful, EmailID Already Exists", Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-                            }
-                        }
-                    });
-        }
-    }
-
-    private void SignInUser()
-    {
-        String email = signInEmail.getText().toString();
-        String password = signInPassword.getText().toString();
-
-        if(TextUtils.isEmpty(email))
-        {
-            Toast.makeText(this, "Please Enter Email", Toast.LENGTH_SHORT).show();
-            signInEmail.requestFocus();
-        }
-        else if (TextUtils.isEmpty(password))
-        {
-            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
-            signInPassword.requestFocus();
-        }
-        else
-        {
-            loadingBar.setTitle("Login Account");
-            loadingBar.setMessage("Please wait. While we are checking credentials");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(SignIn_Activity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful())
-                            {
-                                //AddUserToDatabase();
-                                Intent intent = new Intent(SignIn_Activity.this, Home_Activity.class);
-                                startActivity(intent);
-                            }
-                            else
-                            {
-                                Toast.makeText(SignIn_Activity.this, "Account with this EmailID does not exists", Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-                            }
-                        }
-                    });
-        }
-    }
-
-    private void SignInWithGoogle()
-    {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            if (account != null) {
-                AddUserToDatabase();
-                Intent intent = new Intent(SignIn_Activity.this, Home_Activity.class);
+                Intent intent = new Intent(SignIn_Activity.this,Home_Activity.class);
                 startActivity(intent);
+                finish();
             }
-        } catch (ApiException e) {
-            Toast.makeText(SignIn_Activity.this, "signInResult:failed code=" + e.getStatusCode(), Toast.LENGTH_SHORT).show();
-            loadingBar.dismiss();
-        }
-    }
-
-    private void AddUserToDatabase()
-    {
-        FirebaseUser User = mAuth.getCurrentUser();
-        String userID = User.getUid();
-        String email = User.getEmail();
-
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
-
-        RootRef.child("Users").child(userID).child("email").setValue(email);
+        });
     }
 }
