b8e03fb4b20b142acf550876528aa8af492cc165
arbaz <arbaz@oraan.com>
2019-08-31 19:37:33 +0500

    camera interaction and capture work

---
+++ app/src/main/java/com/maaz/interiar/ui/activities/CaptureImageActivity.java
@@ -0,0 +1,27 @@
+package com.maaz.interiar.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.maaz.interiar.R;
+import com.maaz.interiar.utils.Constant;
+
+public class CaptureImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_image);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageView imageViewCaptureImage = findViewById(R.id.imageViewCaptureImage);
+        Uri imageUri = Uri.parse(getIntent().getStringExtra(Constant.IMAGE_DATA));
+
+        imageViewCaptureImage.setImageURI(imageUri);
+    }
+}
--- app/src/main/java/com/maaz/interiar/ui/fragments/CameraFragment.java
+++ app/src/main/java/com/maaz/interiar/ui/fragments/CameraFragment.java
@@ -1,19 +1,32 @@
 package com.maaz.interiar.ui.fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.maaz.interiar.R;
+import com.maaz.interiar.ui.activities.CaptureImageActivity;
+import com.maaz.interiar.utils.Constant;
+import com.maaz.interiar.utils.Utility;
+import com.otaliastudios.cameraview.BitmapCallback;
+import com.otaliastudios.cameraview.CameraListener;
 import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.PictureResult;
 
-public class CameraFragment extends Fragment {
+public class CameraFragment extends BaseFragment implements View.OnClickListener {
+
+    private CameraView cameraView;
+    private Button buttonCapture;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,9 +34,80 @@ public class CameraFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        CameraView cameraView = view.findViewById(R.id.cameraView);
-        cameraView.setLifecycleOwner(getViewLifecycleOwner());
+        initialize(view);
     }
+
+    private void initialize(final View view) {
+
+        buttonCapture = view.findViewById(R.id.buttonCapture);
+
+        cameraView = view.findViewById(R.id.cameraView);
+        cameraView.setLifecycleOwner(getViewLifecycleOwner());
+
+        buttonCapture.setOnTouchListener(colorFilterOnTouch);
+        buttonCapture.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case (MotionEvent.ACTION_DOWN & MotionEvent.ACTION_BUTTON_PRESS): {
+                        buttonCapture.setScaleY(1.6f);
+                        buttonCapture.setScaleX(1.6f);
+                        break;
+                    }
+                    case (MotionEvent.ACTION_UP & MotionEvent.ACTION_CANCEL): {
+                        buttonCapture.setScaleY(1.0f);
+                        buttonCapture.setScaleX(1.0f);
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+        cameraView.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(@NonNull PictureResult result) {
+                super.onPictureTaken(result);
+                result.toBitmap(new BitmapCallback() {
+                    @Override
+                    public void onBitmapReady(@Nullable Bitmap bitmap) {
+                        assert bitmap != null;
+                        processImageFromBitmapAndStartImageActivity(bitmap);
+                    }
+                });
+            }
+        });
+        setListeners();
+    }
+
+    private void setListeners() {
+        buttonCapture.setOnClickListener(this);
+    }
+
+    private void processImageFromBitmapAndStartImageActivity(Bitmap bitmap) {
+        buttonCapture.setOnClickListener(CameraFragment.this);
+        buttonCapture.setScaleY(1.0f);
+        buttonCapture.setScaleX(1.0f);
+        Uri imageUri = Utility.getImageUri(requireContext(), bitmap);
+        Intent imageIntent = new Intent(requireActivity(), CaptureImageActivity.class);
+        imageIntent.putExtra(Constant.IMAGE_DATA, imageUri.toString());
+        requireActivity().startActivity(imageIntent);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonCapture: {
+                if (checkOrAskForMultipleRunTimePermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    buttonCapture.setOnClickListener(null);
+                    cameraView.takePicture();
+                }
+                break;
+            }
+        }
+    }
+
 }
