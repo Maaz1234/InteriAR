18aa626a7721eab93e72c72a3d0f5d03afeaa1a2
Hira Ibrahim <hiraibrahim197@gmail.com>
2019-09-04 20:40:35 +0500

    -

--- app/src/main/java/com/maaz/interiar/ui/Partner/AddNewProductByCategoryActivity.java
+++ app/src/main/java/com/maaz/interiar/ui/Partner/AddNewProductByCategoryActivity.java
@@ -12,5 +12,6 @@ public class AddNewProductByCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_product_by_category);
+
     }
 }
