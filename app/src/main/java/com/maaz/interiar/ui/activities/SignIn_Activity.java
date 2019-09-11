8b354ddbee26e394fba46605e3e77cd8b509a5d2
Maaz Maqsood <maaz19972016@gmail.com>
2019-09-11 08:55:28 +0500

    Worked on: -- Faceboook Login (Incomplete).

--- app/src/main/java/com/maaz/interiar/ui/activities/SignIn_Activity.java
+++ app/src/main/java/com/maaz/interiar/ui/activities/SignIn_Activity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,22 +18,36 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.facebook.FacebookSdk;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.maaz.interiar.R;
 import com.maaz.interiar.ui.Partner.PartnerSignInActivity;
+
+import java.util.HashMap;
 
 public class SignIn_Activity extends AppCompatActivity {
 
@@ -40,13 +55,15 @@ public class SignIn_Activity extends AppCompatActivity {
     private RadioButton radioSigninBtn, radioSignupBtn;
     private TextView forgetPassword, divider_text, become_partner_text;
     private EditText signInEmail, signInPassword, signUpName, signUpEmail, signUpPassword;
-    private Button loginButton, googleButton, facebookButton;
+    private Button loginButton, googleButton;
+    private LoginButton facebookButton;
     private ProgressDialog loadingBar;
-    //private String parentDBName = "Users";
+    private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth mAuth;
+    private CallbackManager callbackManager;
     private GoogleSignInClient mGoogleSignInClient;
     private static final int RC_SIGN_IN = 9001;
-    private boolean checkGoogleOrFbSignIn;
+    private boolean checkGoogleOrFbSignIn = false;
     String personId, personName, personEmail;
     Uri personPhoto;
     private String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+";
@@ -56,25 +73,25 @@ public class SignIn_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_);
 
-        radioGroup = (RadioGroup) findViewById(R.id.radio_group);
-        radioSigninBtn = (RadioButton) findViewById(R.id.radio_signin_button);
-        radioSignupBtn = (RadioButton) findViewById(R.id.radio_signup_button);
-        forgetPassword = (TextView) findViewById(R.id.forget_password);
-        divider_text = (TextView) findViewById(R.id.divider_txt);
-        signInEmail = (EditText) findViewById(R.id.signIn_email);
-        signInPassword = (EditText) findViewById(R.id.signIn_password);
-        signUpName = (EditText) findViewById(R.id.signUp_name);
-        signUpEmail = (EditText) findViewById(R.id.signUp_email);
-        signUpPassword = (EditText) findViewById(R.id.signUp_password);
-        loginButton = (Button) findViewById(R.id.login_btn);
+        radioGroup = findViewById(R.id.radio_group);
+        radioSigninBtn = findViewById(R.id.radio_signin_button);
+        radioSignupBtn = findViewById(R.id.radio_signup_button);
+        forgetPassword = findViewById(R.id.forget_password);
+        divider_text = findViewById(R.id.divider_txt);
+        signInEmail = findViewById(R.id.signIn_email);
+        signInPassword = findViewById(R.id.signIn_password);
+        signUpName = findViewById(R.id.signUp_name);
+        signUpEmail = findViewById(R.id.signUp_email);
+        signUpPassword = findViewById(R.id.signUp_password);
+        loginButton = findViewById(R.id.login_btn);
         googleButton = findViewById(R.id.google_button);
         facebookButton = findViewById(R.id.fb_button);
         loadingBar = new ProgressDialog(this);
-        //Paper.init(this);
-        become_partner_text = (TextView) findViewById(R.id.txt_Partner);
+        become_partner_text = findViewById(R.id.txt_Partner);
 
 
         mAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
@@ -83,20 +100,8 @@ public class SignIn_Activity extends AppCompatActivity {
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
 
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-
-        if (currentUser != null || account != null)
-        {
-            loadingBar.setTitle("Already Logged in");
-            loadingBar.setMessage("Please wait...");
-            loadingBar.setCanceledOnTouchOutside(false);
-            loadingBar.show();
-            Intent intent = new Intent(SignIn_Activity.this, HomeActivity.class);
-            startActivity(intent);
-            loadingBar.dismiss();
-        }
+        //checkingForAlreadyLoggedInUser();
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -149,7 +154,35 @@ public class SignIn_Activity extends AppCompatActivity {
         {
             @Override
             public void onClick(View v) {
+                loadingBar.setTitle("Logging in with Google");
+                loadingBar.setMessage("Please Wait...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
                 SignInWithGoogle();
+            }
+        });
+
+        callbackManager = CallbackManager.Factory.create();
+
+        facebookButton.setReadPermissions("email", "public_profile");
+
+        facebookButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult)
+            {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel()
+            {
+
+            }
+
+            @Override
+            public void onError(FacebookException error)
+            {
+
             }
         });
 
@@ -157,8 +190,8 @@ public class SignIn_Activity extends AppCompatActivity {
         {
             @Override
             public void onClick(View view) {
-                loadingBar.setTitle("Creating Account");
-                loadingBar.setMessage("Please wait. While we are checking credentials");
+                loadingBar.setTitle("Loading");
+                loadingBar.setMessage("Please wait...");
                 loadingBar.setCanceledOnTouchOutside(false);
                 loadingBar.show();
                 Intent i = new Intent(SignIn_Activity.this, PartnerSignInActivity.class);
@@ -167,6 +200,28 @@ public class SignIn_Activity extends AppCompatActivity {
             }
         });
     }
+
+    ////// Function for checking if user is already logged in
+
+    private void checkingForAlreadyLoggedInUser()
+    {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null || account != null)
+        {
+            loadingBar.setTitle("Already Logged in");
+            loadingBar.setMessage("Please wait...");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            Intent intent = new Intent(SignIn_Activity.this, HomeActivity.class);
+            startActivity(intent);
+            loadingBar.dismiss();
+        }
+    }
+
+    ////// Function for signing up the new user
 
     private void SignUpUser()
     {
@@ -214,18 +269,19 @@ public class SignIn_Activity extends AppCompatActivity {
                             {
                                 personName = name;
                                 AddUserToDatabase();
-                                Intent intent = new Intent(SignIn_Activity.this, HomeActivity.class);
-                                startActivity(intent);
                             }
                             else
                             {
-                                Toast.makeText(SignIn_Activity.this, "SignUp UnSuccessful, EmailID Already Exists", Toast.LENGTH_SHORT).show();
+                                String Error = task.getException().getMessage();
+                                Toast.makeText(SignIn_Activity.this, Error, Toast.LENGTH_SHORT).show();
                                 loadingBar.dismiss();
                             }
                         }
                     });
         }
     }
+
+    ////// Function for signing in the user
 
     private void SignInUser()
     {
@@ -269,15 +325,21 @@ public class SignIn_Activity extends AppCompatActivity {
         }
     }
 
+    ////// Functions for google sign in
+
     private void SignInWithGoogle()
     {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        loadingBar.dismiss();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
@@ -288,17 +350,12 @@ public class SignIn_Activity extends AppCompatActivity {
         }
     }
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        loadingBar.setTitle("Login Account");
-        loadingBar.setMessage("Please wait. While we are checking credentials");
-        loadingBar.setCanceledOnTouchOutside(false);
-        loadingBar.show();
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
             if (account != null) {
                 checkGoogleOrFbSignIn = true;
+                loadingBar.dismiss();
                 AddUserToDatabase();
-                Intent intent = new Intent(SignIn_Activity.this, HomeActivity.class);
-                startActivity(intent);
             }
         } catch (ApiException e) {
             Toast.makeText(SignIn_Activity.this, "signInResult:failed code=" + e.getStatusCode(), Toast.LENGTH_SHORT).show();
@@ -306,9 +363,34 @@ public class SignIn_Activity extends AppCompatActivity {
         }
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            AddUserToDatabase();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignIn_Activity.this, "signInResult:failed code=" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    ////// Function for adding user to database
+
     private void AddUserToDatabase()
     {
-        if (checkGoogleOrFbSignIn = true)
+        HashMap<Object,String> userdata = new HashMap<>();
+
+        if (checkGoogleOrFbSignIn == true)
         {
             GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(SignIn_Activity.this);
             personName = acct.getDisplayName();
@@ -325,11 +407,42 @@ public class SignIn_Activity extends AppCompatActivity {
             personPhoto = null;
         }
 
-        final DatabaseReference RootRef;
-        RootRef = FirebaseDatabase.getInstance().getReference();
+        userdata.put("Name",personName);
+        userdata.put("Email",personEmail);
+        userdata.put("UserID",personId);
 
-        RootRef.child("Users").child(personId).child("email").setValue(personEmail);
+//        final DatabaseReference RootRef;
+//        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        firebaseFirestore.collection("Users")
+                .document(personId)
+                .set(userdata)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent intent = new Intent(SignIn_Activity.this, HomeActivity.class);
+                        startActivity(intent);
+                        loadingBar.dismiss();
+                    }
+                });
+
+/*                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isSuccessful())
+                        {
+                            Intent intent = new Intent(SignIn_Activity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            String Error = task.getException().getMessage();
+                            Toast.makeText(SignIn_Activity.this, Error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });*/
+
+        //RootRef.child("Users").child(personId).child("email").setValue(personEmail);
         //RootRef.child("Users").child(personId).child("picture").setValue(personPhoto);
     }
 }
-//push recheck
