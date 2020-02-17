2fdfdc5d43db8c3696278c56ed9fb68e30471c22
Zaibi <zohaibamjad94@gmail.com>
2020-02-18 02:52:13 +0500

    notification recycler in progress

--- app/src/main/java/com/maaz/interiar/ui/fragments/UserNotificationsFragment.java
+++ app/src/main/java/com/maaz/interiar/ui/fragments/UserNotificationsFragment.java
@@ -2,14 +2,17 @@ package com.maaz.interiar.ui.fragments;
 
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.maaz.interiar.R;
+import com.maaz.interiar.ui.adapters.NotificationListAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,8 +28,14 @@ public class UserNotificationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        RecyclerView notificationlist = (RecyclerView) findViewById.(R.id.notification_list);
+        notificationlist.setLayoutManager(new LinearLayoutManager(this));
+        String[] language = {"new product","50% discount","habit's new arrival","update","office collection","italian lamps","single beds"};
+        notificationlist.setAdapter(new NotificationListAdapter(language));
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_notifications, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_notifications, container, false);
+     return view;
     }
 
 }
